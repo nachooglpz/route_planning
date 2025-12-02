ac67a3419b0910ae5d7845ae37c7a37530dd80f9
Luis Ignacio <a01644423@tec.mx>
2025-12-02 17:12:53 -0600

    search problem formulation

--- component2_route_planner.py
+++ component2_route_planner.py
@@ -1,5 +1,36 @@
+from simpleai.search import breadth_first, depth_first, uniform_cost, iterative_limited_depth_first, astar, SearchProblem
 import osmnx as ox
 import pyproj
+
+# Search problem definition
+class RoutePlanning(SearchProblem):
+    def __init__(self, i_id, e_id, G):
+        SearchProblem.__init__(self, i_id)
+        self.goal = e_id
+        self.G = G
+
+    def actions(self, state):
+        return list(self.G.neighbors(state))
+    
+    def result(self, state, action):
+        return action
+    
+    def is_goal(self, state):
+        return state == self.goal
+    
+    def cost(self, state, action, state2):
+        data = list(self.G.get_edge_data(state, state2).values())[0]
+        return data.get('length', 1)
+    
+    def heuristic(self, state):
+        # using euclidean distance
+        x1 = self.G.nodes[state]['x']
+        y1 = self.G.nodes[state]['y']
+
+        x2 = self.G.nodes[self.goal]['x']
+        y2 = self.G.nodes[self.goal]['y']
+
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 # get projection coordinates from graph
 def project_point(lat, lon, G_proj):
