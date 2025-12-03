9a93beec9d91bb1c57bde35f8f0d8d2c515eec70
Luis Ignacio <a01644423@tec.mx>
2025-12-03 16:01:10 -0600

    long range implemented

--- component2_route_planner.py
+++ component2_route_planner.py
@@ -4,7 +4,7 @@ from kd_tree import KDTree
 import pyproj, time
 from func_timeout import func_timeout, FunctionTimedOut
 
-# Search problem definition
+# search problem definition
 class RoutePlanning(SearchProblem):
     def __init__(self, i_id, e_id, G):
         SearchProblem.__init__(self, i_id)
@@ -61,8 +61,7 @@ print('------------------------------------')
 
 short_range_points = [(20.672415270083093, -103.35694735274907), (20.67451623525472, -103.3590330613026),
                       (20.673038501017935, -103.36236405280633), (20.67188488248092, -103.36308214722402),
-                      (20.671211252700303, -103.35984558966872)
-]
+                      (20.671211252700303, -103.35984558966872)]
 
 astar_times = []
 bfs_times = []
@@ -139,9 +138,8 @@ print('MID RANGE DISTANCES')
 print('------------------------------------')
 
 mid_range_points = [(20.68003419605609, -103.34978764582817), (20.65861100742661, -103.35914622492686),
-                      (20.670817895239068, -103.37691317671931), (20.660220704521567, -103.36591689816883),
-                      (20.67170125043079, -103.34258090216602)
-]
+                    (20.670817895239068, -103.37691317671931), (20.660220704521567, -103.36591689816883),
+                    (20.67170125043079, -103.34258090216602)]
 
 astar_times = []
 bfs_times = []
@@ -197,6 +195,87 @@ for i, (lat, lon) in enumerate(mid_range_points):
         astar_solution = func_timeout(TIMEOUT, astar, args=(rp,))
     except FunctionTimedOut:
         astar_solution = None
+    astar_times.append(time.time() - start_time)
+
+    # print the output for this node nicely
+    parts = [
+    f"bfs: {bfs_times[i]:.6f} s" if bfs_solution else "",
+    f"dfs: {dfs_times[i]:.6f} s" if dfs_solution else "",
+    f"ucs: {ucs_times[i]:.6f} s" if ucs_solution else "",
+    f"iddfs: {iddfs_times[i]:.6f} s" if iddfs_solution else "",
+    f"astar: {astar_times[i]:.6f} s" if astar_solution else "",
+    ]
+    output = ", ".join(filter(None, parts))
+    print(output)
+
+
+print(f"bfs avg:   {(sum(bfs_times) / len(bfs_times)):.6f} s")
+print(f"dfs avg:   {(sum(dfs_times) / len(dfs_times)):.6f} s")
+print(f"ucs avg:   {(sum(ucs_times) / len(ucs_times)):.6f} s")
+print(f"iddfs avg: {(sum(iddfs_times) / len(iddfs_times)):.6f} s")
+print(f"astar avg: {(sum(astar_times) / len(astar_times)):.6f} s")
+
+print('------------------------------------')
+print('LARGE RANGE DISTANCES')
+print('------------------------------------')
+
+large_range_points = [(20.69381511190883, -103.35149193509012), (20.676533932372752, -103.32373059890227),
+                    (20.659550509767612, -103.3458862806719), (20.658677119431214, -103.37492739973364),
+                    (20.67607236812044, -103.3235355446329)]
+
+TIMEOUT = 0.5
+
+astar_times = []
+bfs_times = []
+dfs_times = []
+ucs_times = []
+iddfs_times = []
+
+x, y = project_point(coordinates[0], coordinates[1], G_proj)
+e_point, e_id = kdtree.nearest_neighbor((x, y))
+
+for i, (lat, lon) in enumerate(large_range_points):
+
+    x, y = project_point(lat, lon, G_proj)
+    i_point, i_id = kdtree.nearest_neighbor((x, y))
+
+    rp = RoutePlanning(i_id, e_id, G_proj)
+
+    # BFS
+    start_time = time.time()
+    try:
+        bfs_solution = func_timeout(TIMEOUT, breadth_first, args=(rp,))
+    except FunctionTimedOut:
+        bfs_solution = None
+    bfs_times.append(time.time() - start_time)
+
+    # DFS
+    start_time = time.time()
+    try:
+        dfs_solution = func_timeout(TIMEOUT, depth_first, args=(rp,))
+    except FunctionTimedOut:
+        dfs_solution = None
+    dfs_times.append(time.time() - start_time)
+
+    # UCS
+    start_time = time.time()
+    try:
+        ucs_solution = func_timeout(TIMEOUT, uniform_cost, args=(rp,))
+    except FunctionTimedOut:
+        ucs_solution = None
+    ucs_times.append(time.time() - start_time)
+
+    # IDDFS
+    start_time = time.time()
+    try:
+        iddfs_solution = func_timeout(TIMEOUT, iterative_limited_depth_first, args=(rp,))
+    except FunctionTimedOut:
+        iddfs_solution = None
+    iddfs_times.append(time.time() - start_time)
+
+    # ASTAR
+    start_time = time.time()
+    astar_solution = astar(rp)
     astar_times.append(time.time() - start_time)
 
     # print the output for this node nicely
